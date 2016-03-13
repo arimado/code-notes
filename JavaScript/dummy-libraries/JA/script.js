a813d6973f55ab89e22a4235e7c8b31d6ce964c5
Jeremy Arimado <jarimado@gmail.com>
2016-03-13 12:05:45 +1100

    time starts now

--- JavaScript/dummy-libraries/JA/script.js
+++ JavaScript/dummy-libraries/JA/script.js
@@ -3,8 +3,12 @@
 JA = {};
 
 // MEGA MAP
-// takes an array or object and maps it to the
-// 11.59
+// takes an array/object and a mapped version of the input array/object
+// - dont include prototype properties
+//      - use hasOwnProperty to screen out of callback
+//      -
+// - don't let it accept anything other than an array/object
+// - get rid of the else statement
 
 JA.MEGAMAP = function (collection) {
     var result = [];
