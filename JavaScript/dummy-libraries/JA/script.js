185dec69e4e1b68aa65e0cb4aea8990658156a47
Jeremy Arimado <jarimado@gmail.com>
2016-03-13 16:48:02 +1100

    addded detailed comments

--- JavaScript/dummy-libraries/JA/script.js
+++ JavaScript/dummy-libraries/JA/script.js
@@ -5,44 +5,9 @@ JA = {};
 var one23_OBJ = { one: 1, "two": 2, three: 3 };
 var one23_ARR = [1, 2, 3];
 
-
-// MEGA MAP
-// takes an array/object and a mapped version of the input array/object
-// - dont include prototype properties
-//      - use hasOwnProperty to screen out of callback
-//      -asd
-// - don't let it accept anything other than an array/object
-// - get rid of the else statement
-
-
-
 var modifyVal = function (val) {
     return val + ' - modified';
 }
-
-JA.isPlainObject = function (o) { // FROM STACKOVERFLOW
-   return ((o === null) || Array.isArray(o) || typeof o == 'function') ?
-          false
-          :(typeof o == 'object');
-}
-
-JA.MEGAMAP_old = function (collection, callback) {
-    var result;
-    if (Array.isArray(collection)) {
-        result = [];
-        for(var i = 0; i < collection.length; i += 1) {
-            result.push(callback(collection[i]));
-        }
-    } else if (JA.isPlainObject(collection)) {
-        result = Object.create(null);
-        for (var key in collection) {
-            result[key] = callback(collection[key]);
-        }
-    } else {
-        console.log('MEGAMAP only accepts Arrays or Objects');
-    }
-    return result;
-};
 
 // Most techniques used from the below function was stolen from:
 // http://stackoverflow.com/questions/14810506/map-function-for-objects-instead-of-arrays
@@ -59,13 +24,18 @@ JA.MEGAMAP = function (collection, callback) {
     isArray ? dataType = [] : dataType = Object.create(null);
 
     // use Object.keys and reduce to operate on both arrays and objects
+    // Object.keys returns an array of keys for objects and indexes for arrays
+    // Array.prototype.reduce can be used to push elements to an array or objects
+    // when the initial value is either an array or object
+    // you would then just use bracket notation to simulate pushing to the array/object
+
     result = Object.keys(collection).reduce(function(previous, current) {
         previous[current] = callback(collection[current]);
         return previous;
     }, dataType);
 
     return result;
-}; 
+};
 
 var returnedObject = JA.MEGAMAP(one23_OBJ, modifyVal);
 
