94a7191ec19fc8434c6d1787926e702a403401ab
Jeremy Arimado <jarimado@gmail.com>
2016-03-14 20:29:56 +1100

    Project and flatten with nested maps and concatAll()

--- JavaScript/functional-programming-js/js/script.js
+++ JavaScript/functional-programming-js/js/script.js
@@ -12,3 +12,68 @@ Array.prototype.concatAll = function() {
 };
 
 console.log(JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll()));
+
+
+// Project and Flatten array using nested maps and
+
+(function () {
+	var movieLists = [
+			{
+				name: "New Releases",
+				videos: [
+					{
+						"id": 70111470,
+						"title": "Die Hard",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"bookmark": []
+					},
+					{
+						"id": 654356453,
+						"title": "Bad Boys",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"bookmark": [{ id:432534, time:65876586 }]
+					}
+				]
+			},
+			{
+				name: "Dramas",
+				videos: [
+					{
+						"id": 65432445,
+						"title": "The Chamber",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"bookmark": []
+					},
+					{
+						"id": 675465,
+						"title": "Fracture",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"bookmark": [{ id:432534, time:65876586 }]
+					}
+				]
+			}
+		];
+
+	// ------------   INSERT CODE HERE!  -----------------------------------
+	// Use map and concatAll to flatten the movieLists in a list of video ids.
+
+    var j = movieLists.map(function (list) {
+         return list.videos.map(function (video){
+             console.log(video.id);
+             return video.id;
+         });
+     }) 
+
+	// ------------   INSERT CODE HERE!  -----------------------------------
+
+	return movieLists // Complete this expression!
+
+})();
