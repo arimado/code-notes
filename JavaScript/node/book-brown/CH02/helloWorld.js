6895ef12e059b7f4d001da42b633d8ef5ddf7fe4
doloreel <jarimado@gmail.com>
2016-04-29 11:38:04 +1000

    [node] Serving files: stuck on logo

--- JavaScript/node/book-brown/CH02/helloWorld.js
+++ JavaScript/node/book-brown/CH02/helloWorld.js
@@ -1,9 +1,9 @@
-var http = require('http');
-
+var http = require('http'); // What is this?
+var fs = require('fs');     // What is fs?
 
 var serverStaticFile = function (res, path, contentType, responseCode) {
     if(!responseCode) responseCode = 200;
-    fs.readFile(_dirname + path, function(err, data){
+    fs.readFile(__dirname + path, function(err, data){
         if (err) {
             res.writeHead(500, {'Content-Type':'text/plain'});
             res.end('500 - ERROR BRAH!');
@@ -18,11 +18,16 @@ http.createServer(function(req, res) {
     var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
     switch(path) {
         case '':
-            serverStaticFile(res, '/public/')
+            serverStaticFile(res, '/public/home.html', 'text/html');
             break;
         case '/about':
+            serverStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo.jpg':
+            serverStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
             break;
         default:
+            serverStaticFile(res, '/public/404.html', 'text/html');
             break;
     }
 }).listen(3000);
