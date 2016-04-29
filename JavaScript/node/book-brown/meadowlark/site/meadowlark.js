b3f78f0327260ac41d61046d133ed8dd1bb6ca41
doloreel <jarimado@gmail.com>
2016-04-29 19:02:39 +1000

    [node] added public files

--- JavaScript/node/book-brown/meadowlark/site/meadowlark.js
+++ JavaScript/node/book-brown/meadowlark/site/meadowlark.js
@@ -5,6 +5,8 @@ var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/public'));  
+
 app.set('port', process.env.PORT || 3000);
 
 
@@ -30,6 +32,7 @@ app.use(function (err, req, res, next) {
     res.status(500);
     res.render('500');
 });
+
 
 app.listen(app.get('port'), function() {
     console.log('Express started on tests.loc:' + app.get('port') + '; press Ctrl-C to terminate.');
