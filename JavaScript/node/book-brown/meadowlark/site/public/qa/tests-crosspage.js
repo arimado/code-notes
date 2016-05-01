b5f821ad41ea928c5b4a7d69a6b6599b65723dda
Jeremy Arimado <jarimado@gmail.com>
2016-05-01 14:42:10 +1000

    [skip][node] Cross-page QA:cannot find proper method to fetch input field text

--- JavaScript/node/book-brown/meadowlark/site/public/qa/tests-crosspage.js
+++ JavaScript/node/book-brown/meadowlark/site/public/qa/tests-crosspage.js
@@ -1,42 +1,51 @@
+
+
+
+
+
+
+
+
+
 var Browser = require('zombie');
 var assert = require('chai').assert // what's going on here mate?
 
 var browser;
 
 suite('Cross-Page Tests', function () {
-
     // OOAAH
     setup(function () {
         browser = new Browser();
     });
 
-    test('requesting hood-river should populate the refferrer',
-    function(done) {
+    test('requesting hood-river should populate the refferrer', function(done) {
         var referrer = 'http://test.loc:3000/tours/hood-river';
         browser.visit(referrer, function() {
+            console.log('visited!');
             browser.clickLink('.requestGroupRate', function() {
-                assert(browser.field('referrer').value === referrer);
+                console.log('clicked the link!');
+                assert(browser.resources[0].request.headers._headers[0][1] === referrer);
                 done();
             });
         });
     });
 
-    test('requesting from oregon should populate the referrer',
-    function(done){
+    test('requesting from oregon should populate the referrer', function(done){
         var referrer = 'http://test.loc:3000/tours/oregon-coast';
         browser.visit(referrer, function() {
             browser.clickLink('.requestGroupRate', function() {
-                assert(browser.field('referrer').value === referrer);
+                assert(browser.resources[0].request.headers._headers[0][1] === referrer);
                 done();
             });
         });
     });
 
-    test('visiting the "request group thing" should NOT populate the referrer', function(done){
+    test('visiting the "request group thing" should NOT populate the referrer',
+    function(done){
         var referrer = 'http://test.loc:3000/tours/request-group-rate';
         browser.visit(referrer, function() {
             browser.clickLink('.requestGroupRate', function() {
-                assert(browser.field('referrer').value === referrer);
+                assert(browser.field('referrer').value === '');
                 done();
             });
         });
