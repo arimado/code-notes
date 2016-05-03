6f5742851960c514ce611328c514370335eaf151
doloreel <jarimado@gmail.com>
2016-05-03 16:16:39 +1000

    *stuck*[node][form upload] Uploading file to the server

--- JavaScript/node/book-brown/meadowlark/site/meadowlark.js
+++ JavaScript/node/book-brown/meadowlark/site/meadowlark.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var fortune = require('./lib/fortune.js'); // prefixed with dot or else require would look in node-modules
+var bodyparserInvoke = require('body-parser')(); // why is this invoked ???
+var formidable = require('formidable');
+var jqupload = require('jquery-file-upload-middleware');
 
 var handlebars = require('express-handlebars').create({
     defaultLayout: 'main',
@@ -25,6 +28,73 @@ app.use(function(req, res, next) {
     res.locals.showTests = isTesting;
     next();
 })
+
+// jQuery file upload
+
+app.use('/upload', function(req, res, next){
+    var now = Date.now();
+    jqupload.fileHandler({
+    uploadDir: function(){
+        return __dirname + '/public/uploads/' + now;
+    },
+    uploadUrl: function(){
+        return '/uploads/' + now; },
+    })(req, res, next); // self-invoked with req,res,next
+});
+
+
+// Bodyparser
+app.use(bodyparserInvoke);
+
+app.get('/newsletter', function(req, res) {
+    res.render('newsletter', {csrf: 'CSRF Token goes here'});
+});
+
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html') === 'json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+        console.log('req.query.form: ' + req.query.form);
+        console.log('req.body._csrf :' + req.body._csrf);
+        console.log('req.body.name: ' + req.body.name);
+        console.log('req.body.email: ' + req.body.email);
+
+    } else {
+        // if there were an error, we would redirect to an error page
+        console.log('req.query.form: ' + req.query.form);
+        console.log('req.body._csrf :' + req.body._csrf);
+        console.log('req.body.name: ' + req.body.name);
+        console.log('req.body.email: ' + req.body.email);
+        res.redirect(303, '/thank-you');
+    }
+});
+
+app.get('/thank-you', function(req, res) {
+    res.render('thank-you');
+});
+
+
+// Vaction photo
+app.get('/contest/vacation-photo',function(req,res){
+    var now = new Date();
+    res.render('contest/vacation-photo',{
+            year: now.getFullYear(),
+            month: now.getMonth()
+    });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
+
 
 // ****************************************
 // LOGIC ----------------------------------
